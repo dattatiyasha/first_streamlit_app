453570b4f2c63e2c99e56669d1af342af9b828ab
dattatiyasha <103967773+dattatiyasha@users.noreply.github.com>
2022-04-19 09:42:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My first app')
+streamlit.title('My first Outlet')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
